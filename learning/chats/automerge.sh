a55bb534dd7bb340b04b06b951229a8efb5b359e
yuanguangshan <yuanguangshan@gmail.com>
2025-07-10 16:39:38 +0800

    add

--- learning/chats/automerge.sh
+++ learning/chats/automerge.sh
@@ -2,13 +2,13 @@
 # merge new features from dev to main
 # 1. checkout main
 git checkout main
-sleep 2
-# 2. merge dev
+sleep 3
+# 2. merge dev,这步可能会冲突
 git merge dev -m 'main:get new features from dev '
-sleep 2
-# 3. push main
+sleep 5
+# 3. push main，这步可能耗时较长
 git push
-sleep 2
+sleep 5
 git checkout dev
 # 4. checkout dev
 # 5. 回到原工作分支： dev
