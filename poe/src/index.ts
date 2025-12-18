34f948b45ef9d47110fe58098688c65a15b10e10
yuanguangshan <yuanguangshan@gmail.com>
2025-12-18 10:11:34 +0800

    add

--- poe/src/index.ts
+++ poe/src/index.ts
@@ -76,7 +76,12 @@ export default {
             try {
                 const response = await fetch(proxyRequest);
                 const newHeaders = new Headers(response.headers);
+
+                // Fix "The 'Access-Control-Allow-Origin' header contains multiple values" error
+                newHeaders.delete('Access-Control-Allow-Origin');
                 newHeaders.set('Access-Control-Allow-Origin', '*');
+                newHeaders.set('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS');
+                newHeaders.set('Access-Control-Allow-Headers', 'Content-Type');
 
                 return new Response(response.body, {
                     status: response.status,
